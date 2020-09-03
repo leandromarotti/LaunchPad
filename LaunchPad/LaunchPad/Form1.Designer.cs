68f9bce05bdba6f678337ca63430b858585a0660
Leandro Marotti <53351127+leandromarotti@users.noreply.github.com>
2020-09-03 18:25:42 -0400

    First  LaunchPad commit

---
+++ LaunchPad/LaunchPad/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace LaunchPad
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lstFolders = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btnNewFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnNewFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnExit = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 31);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lstFolders);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lstFiles);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Size = new System.Drawing.Size(522, 480);
+            this.splitContainer1.SplitterDistance = 241;
+            this.splitContainer1.SplitterWidth = 6;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // lstFolders
+            // 
+            this.lstFolders.BackColor = System.Drawing.Color.Lavender;
+            this.lstFolders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstFolders.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstFolders.FormattingEnabled = true;
+            this.lstFolders.HorizontalScrollbar = true;
+            this.lstFolders.ItemHeight = 21;
+            this.lstFolders.Location = new System.Drawing.Point(0, 17);
+            this.lstFolders.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstFolders.Name = "lstFolders";
+            this.lstFolders.Size = new System.Drawing.Size(241, 463);
+            this.lstFolders.TabIndex = 1;
+            this.lstFolders.DoubleClick += new System.EventHandler(this.lstFolders_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Frequently Accessed Folders";
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.BackColor = System.Drawing.Color.GhostWhite;
+            this.lstFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstFiles.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.HorizontalScrollbar = true;
+            this.lstFiles.ItemHeight = 21;
+            this.lstFiles.Location = new System.Drawing.Point(0, 17);
+            this.lstFiles.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(275, 463);
+            this.lstFiles.TabIndex = 0;
+            this.lstFiles.DoubleClick += new System.EventHandler(this.lstFiles_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Frequently Accessed Files";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNewFolder,
+            this.btnNewFile,
+            this.btnExit});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(522, 31);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btnNewFile
+            // 
+            this.btnNewFile.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnNewFile.Name = "btnNewFile";
+            this.btnNewFile.Size = new System.Drawing.Size(82, 25);
+            this.btnNewFile.Text = "New File";
+            this.btnNewFile.Click += new System.EventHandler(this.btnNewFile_Click);
+            // 
+            // btnNewFolder
+            // 
+            this.btnNewFolder.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnNewFolder.Name = "btnNewFolder";
+            this.btnNewFolder.Size = new System.Drawing.Size(102, 25);
+            this.btnNewFolder.Text = "New Folder";
+            this.btnNewFolder.Click += new System.EventHandler(this.btnNewFolder_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(46, 25);
+            this.btnExit.Text = "Exit";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(522, 511);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmMain";
+            this.Text = "LaunchPad for Folders and Files";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox lstFolders;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnNewFile;
+        private System.Windows.Forms.ToolStripMenuItem btnNewFolder;
+        private System.Windows.Forms.ToolStripMenuItem btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LaunchPad/LaunchPad/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace LaunchPad
+{
+
+    public partial class FrmMain : Form
+    {
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        List<string> fileList = new List<string>();
+        List<string> folderList = new List<string>();
+
+        public void PathFileToListAndListBox(string pathFile, ListBox lb, List<string> ls)
+        {
+            StreamReader sr = new StreamReader(pathFile);
+            lb.Items.Clear();
+            ls.Clear();
+            while (sr.Peek() > -1)
+            {
+                ls.Add(sr.ReadLine());
+            }
+            lb.Items.AddRange(ls.ToArray());
+            sr.Close();
+        }
+
+        public void RefreshListBox(ListBox lb, List<string> list)
+        {
+            lb.Items.Clear();
+            lb.Items.AddRange(list.ToArray());
+        }
+
+        public void UpdatePathFile(List<string> list, string pathFile)
+        {
+            StreamWriter sw = new StreamWriter(pathFile, false);
+            foreach (string line in list)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close();
+        }
+
+        public void AddPathToList(List<string> lst, string item)
+        {
+            // avoid duplicate items
+            if (lst.Contains(item))
+            {
+                lst.Remove(item);
+            }
+            lst.Insert(0, item); // item is the most recent path
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            string specialFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory.ToString(), "LaunchPadFiles");
+            string MRUfilesPath = Path.Combine(specialFolder, "MRUfiles.txt");
+            string MRUfoldersPath = Path.Combine(specialFolder, "MRUfolders.txt");
+            if (!Directory.Exists(specialFolder))
+            {
+                Directory.CreateDirectory(specialFolder);
+            }
+            if (!File.Exists(MRUfilesPath))
+            {
+                File.CreateText(MRUfilesPath);
+            }
+            if (!File.Exists(MRUfoldersPath))
+            {
+                File.CreateText(MRUfoldersPath);
+            }
+            PathFileToListAndListBox(MRUfoldersPath, lstFolders, folderList);
+            PathFileToListAndListBox(MRUfilesPath, lstFiles, fileList);
+            if (!Size.Equals(Properties.Settings.Default.appSize, new Size(0, 0)))
+            {
+                this.Size = Properties.Settings.Default.appSize;
+            }
+            if (!Point.Equals(Properties.Settings.Default.appLocation, new Point(0, 0)))
+            {
+                this.Location = Properties.Settings.Default.appLocation;
+            }
+
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string specialFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory.ToString(), "LaunchPadFiles");
+            string MRUfilesPath = Path.Combine(specialFolder, "MRUFiles.txt");
+            string MRUfoldersPath = Path.Combine(specialFolder, "MRUfolders.txt");
+            UpdatePathFile(fileList, MRUfilesPath);
+            UpdatePathFile(folderList, MRUfoldersPath);
+            Properties.Settings.Default.appSize = this.Size;
+            Properties.Settings.Default.appLocation = this.Location;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnNewFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFile = new OpenFileDialog();
+            oFile.Title = "Choose the file to be launched.";
+            oFile.InitialDirectory = Environment.SpecialFolder.MyComputer.ToString();
+            DialogResult dr = oFile.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string newFile = oFile.FileName;
+                AddPathToList(fileList, newFile);
+                RefreshListBox(lstFiles, fileList);
+                string newFolder = Path.GetDirectoryName(newFile);
+                AddPathToList(folderList, newFolder);
+                RefreshListBox(lstFolders, folderList);
+                //launch the selected file with a default program
+                Process proc = new Process();
+                proc.StartInfo.FileName = newFile;
+                proc.StartInfo.UseShellExecute = true;
+                proc.Start();
+            }
+        }
+
+        private void btnNewFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog oFolder = new FolderBrowserDialog();
+            oFolder.Description = "Choose the folder to be accessed.";
+            oFolder.RootFolder = Environment.SpecialFolder.MyComputer;
+            oFolder.ShowNewFolderButton = true;
+            DialogResult dr = oFolder.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string newFolder = oFolder.SelectedPath;
+                AddPathToList(folderList, newFolder);
+                RefreshListBox(lstFolders, folderList);
+                // open the user selected folder
+                Process proc = new Process();
+                proc.StartInfo.FileName = newFolder;
+                proc.StartInfo.UseShellExecute = true;
+                proc.Start();
+            }
+        }
+
+        private void lstFolders_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstFolders.SelectedItem != null)
+
+                if (Directory.Exists(lstFolders.SelectedItem.ToString()))
+                {
+                    // Open folder in Windows Explorer
+                    Process.Start(@lstFolders.SelectedItem.ToString());
+                }
+                else
+                {
+                    folderList.Remove(lstFolders.SelectedItem.ToString());
+                    RefreshListBox(lstFolders, folderList);
+                }
+
+        }
+
+        private void lstFiles_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstFiles.SelectedItem != null)
+
+                if (File.Exists(lstFiles.SelectedItem.ToString()))
+                {
+                    // launch the selected file
+                    Process proc = new Process();
+                    proc.StartInfo.FileName = lstFiles.SelectedItem.ToString();
+                    proc.StartInfo.UseShellExecute = true;
+                    proc.Start();
+                    AddPathToList(fileList, lstFiles.SelectedItem.ToString());
+                    RefreshListBox(lstFiles, fileList);
+                } 
+                else
+                {
+                    fileList.Remove(lstFiles.SelectedItem.ToString());
+                    RefreshListBox(lstFiles, fileList);
+                }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
